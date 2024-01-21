c6fe3de7e820dc28a17ddba20a18fadc1f8d1d7d
Zouroboros <zouroboros@zoho.eu>
2024-01-21 15:53:17 +0100

    Functions for retrieving the nodes with the highest out degree.

--- src/statistics/mod.rs
+++ src/statistics/mod.rs
@@ -1,17 +1,23 @@
 use std::collections::HashMap;
-use self::summary::{number_of_edges, number_of_nodes};
+use self::summary::{number_of_edges, number_of_nodes, find_nodes_of_max_out_degree};
 
 pub mod summary;
 
 #[derive(Debug)]
-pub struct NetworkStatistics {
+pub struct NetworkStatistics<'a> {
     number_of_nodes: usize,
-    number_of_edges: usize
+    number_of_edges: usize,
+    nodes_of_max_out_degree: Vec<&'a String>,
+    max_out_degree: usize
 }
 
 pub fn gather_statistics(network: &HashMap<String, Vec<String>>) -> NetworkStatistics {
+    let (nodes_of_max_out_degree, max_out_degree) = find_nodes_of_max_out_degree(network);
+
     NetworkStatistics {
         number_of_nodes: number_of_nodes(network),
-        number_of_edges: number_of_edges(network)
+        number_of_edges: number_of_edges(network),
+        nodes_of_max_out_degree,
+        max_out_degree
     }
 }
--- src/statistics/summary.rs
+++ src/statistics/summary.rs
@@ -13,3 +13,19 @@ pub fn number_of_edges(network: &HashMap<String, Vec<String>>) -> usize {
 
     number_of_edges
 }
+
+pub fn find_nodes_of_max_out_degree(network: &HashMap<String, Vec<String>>) -> (Vec<&String>, usize) {
+    let mut max_out_degree = 0;
+    let mut nodes_of_max_out_degree = Vec::new();
+
+    for (node, edges) in network {
+        if edges.len() > max_out_degree {
+            max_out_degree = edges.len();
+            nodes_of_max_out_degree = vec![node];
+        } else if edges.len() == max_out_degree {
+            nodes_of_max_out_degree.push(node)
+        }
+    }
+
+    (nodes_of_max_out_degree, max_out_degree)
+}
